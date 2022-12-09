8170cc57f625727448c80fd7faa7098f67ffa863
Qiansha Meng <mengqianshasha@163.com>
2022-12-08 21:48:41 -0800

    add a few comments

--- ski-app/src/main/java/redis/RedisClient.java
+++ ski-app/src/main/java/redis/RedisClient.java
@@ -24,6 +24,7 @@ public class RedisClient {
 
     public String getTotalVerticalForSomeDay(String resortId, String seasonId, String dayId, String skierId) {
         try (Jedis jedis = this.jedisPool.getResource()) {
+            // Example "resort_season_day_skier:1_1_1_90213:totalVertical"
             String queryKey = "resort_season_day_skier:" + resortId + "_" + seasonId + "_" + dayId + "_" + skierId + ":totalVertical";
             return jedis.get(queryKey);
         } catch (Exception e) {
@@ -36,10 +37,12 @@ public class RedisClient {
     public String getTotalVertical(String skierId, String resortId, String seasonId) {
         try (Jedis jedis = this.jedisPool.getResource()) {
             if (resortId != null && seasonId != null) {
+                // Example "skier_resort_season:90213_1_1:totalVertical"
                 return jedis.get("skier_resort_season:" + skierId + "_" + resortId + "_" + seasonId + ":totalVertical");
             }
 
             if (resortId != null) {
+                // Example "skier_resort:90213_1:totalVertical"
                 return jedis.get("skier_resort:" + skierId + "_" + resortId + ":totalVertical");
             }
         }
@@ -49,6 +52,7 @@ public class RedisClient {
 
     public long getNumOfSkiers(String resortId, String seasonId, String dayId) {
         try (Jedis jedis = this.jedisPool.getResource()) {
+            // Example "resort_season_day:1_1_1:skiersSet"
             String queryKey = "resort_season_day:" + resortId + "_" + seasonId + "_" + dayId + ":skiersSet";
             return jedis.scard(queryKey);
         }
--- ski-consumer/src/main/java/ConsumerWorker.java
+++ ski-consumer/src/main/java/ConsumerWorker.java
@@ -32,10 +32,23 @@ public class ConsumerWorker implements Runnable {
 
             try (Jedis jedis = this.jedisPool.getResource()) {
                 Transaction t = jedis.multi();
+
+                // For GET request: /resorts/{resortID}/seasons/{seasonID}/day/{dayID}/skiers
+                // Schema: resort_season_day:<resortId>_<seasonId>_<dayId>:skiersSet
                 t.sadd("resort_season_day:" + lift.getResortId() + "_" + lift.getSeasonId() + "_" + lift.getDayId() + ":skiersSet", Integer.toString(lift.skierId));
+
+                // For GET request: /skiers/{resortID}/seasons/{seasonID}/days/{dayID}/skiers/{skierID}
+                // Schema: resort_season_day_skier:<resortId>_<seasonId>_<dayId>_<skierId>:totalVertical
                 t.incrBy("resort_season_day_skier:" + lift.getResortId() + "_" + lift.getSeasonId() + "_" + lift.getDayId() + "_" + lift.getSkierId() + ":totalVertical", (long) lift.getLiftId() * 10);
+
+                // For GET request: /skiers/{skierID}/vertical?resort=<resortId>&season=<seasonId>
+                // Schema: skier_resort_season:<skierId>_<resortId>_<seasonId>:totalVertical
                 t.incrBy("skier_resort_season:" + lift.getSkierId() + "_" + lift.getResortId() + "_" + lift.getSeasonId() + ":totalVertical", (long) lift.getLiftId() * 10);
+
+                // For GET request: /skiers/{skierID}/vertical?resort=<resortId>
+                // Schema: skier_resort:<skierId>_<resortId>:totalVertical
                 t.incrBy("skier_resort:" + lift.getSkierId() + "_" + lift.getResortId() + ":totalVertical", (long) lift.getLiftId() * 10);
+
                 t.exec();
             } catch (Exception e) {
                 e.printStackTrace();
